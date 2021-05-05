757cc045b60440c88dc6c045445ee372b019ff21
Lee Wei Chung <weichung.lee@convoy.com.hk>
2021-05-05 15:20:53 +0800

    init commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+const {exec} = require('child_process');
+const async = require('async')
+const port = 6969;
+
+
+server.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+  
+async.series([
+    ()=> {
+        console.info("building resume template");
+        exec('npm run build')
+    },
+    server.listen(port, () => {
+        console.log(`App listening at http://localhost:${port}`);
+    })
+]);
+
+function getDir(dir){
+    return path.join(__dirname,dir);
+};
